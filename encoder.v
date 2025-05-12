84b35e98d158dce36827efc273e426a97790e8c7
mouzarthy <mouzarthy2007@hotmail.com>
2025-05-12 20:07:14 -0300

    tarefa encoder

--- encoder.v
+++ encoder.v
@@ -8,5 +8,43 @@ module Encoder (
     output reg A,
     output reg B
 );
-    
+
+reg [1:0] state;
+
+always @(posedge clk or negedge rst_n) 
+begin
+    if(!rst_n) 
+        begin 
+            state <= 2'b00;
+        end 
+    else 
+        begin
+            if(!horario && antihorario) 
+                begin
+                    case(state)
+                        2'b00: state    = 2'b01;
+                        2'b01: state    = 2'b11;
+                        2'b11: state    = 2'b10;
+                        2'b10: state    = 2'b00;
+                        default: state  = 2'b00;
+                    endcase
+                end 
+            else if(horario && !antihorario) 
+                begin
+                    case(state)
+                        2'b00: state    = 2'b10;
+                        2'b10: state    = 2'b11;
+                        2'b11: state    = 2'b01;
+                        2'b01: state    = 2'b00;
+                        default: state  = 2'b00;
+                    endcase
+                end
+        end
+end
+
+always @(*) begin
+    A = state[1];
+    B = state[0];
+end
+
 endmodule
--- read_encoder.v
+++ read_encoder.v
@@ -7,5 +7,33 @@ module Read_Encoder (
 
     output reg [1:0] dir
 );
-    
+reg A_prev;
+reg B_prev;
+
+reg [3:0] transition;
+
+always @(posedge clk or negedge rst_n) 
+begin
+    if(!rst_n) 
+    begin 
+        A_prev <= 1'b0;
+        B_prev <= 1'b0;
+        dir    <= 2'b00;
+    end 
+    else 
+    begin
+        A_prev <= A;
+        B_prev <= B;
+
+        transition = {A_prev, B_prev, A, B};
+
+        case(transition)
+            4'b0001, 4'b0111, 4'b1110, 4'b1000: dir <= 2'b10;
+            4'b0010, 4'b1011, 4'b1101, 4'b0100: dir <= 2'b01;
+            default: dir <= 2'b00; 
+        endcase
+
+    end
+end
+
 endmodule
